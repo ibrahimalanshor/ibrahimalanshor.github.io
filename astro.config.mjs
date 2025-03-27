c1347698d93c139b3213264cb22d3aaba9fcd470
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-03-27 14:27:44 +0700

    astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon(), mdx()],
   site: 'https://ibrahimalanshor.github.io',
   i18n: {
     locales: ['en', 'id'],
