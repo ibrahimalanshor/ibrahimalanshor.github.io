4458068e9a0e87bdadd6a94a9e0fe2efb6fd97dd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-03-31 16:21:59 +0700

    Aku ingin ke stepa

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { getCollection, type CollectionEntry } from "astro:content";
+import dayjs from "dayjs";
 
 export async function getServices() : Promise<CollectionEntry<'service'>[]> {
     const services = await getCollection('service')
@@ -6,4 +7,12 @@ export async function getServices() : Promise<CollectionEntry<'service'>[]> {
     services.sort((a, b) => a.data.order - b.data.order)
 
     return services
+}
+
+export async function getPosts() : Promise<CollectionEntry<'blog'>[]> {
+    const blogs = await getCollection('blog')
+
+    blogs.sort((a, b) => dayjs(a.data.date).isAfter(b.data.date) ? -1 : 0)
+
+    return blogs
 }
